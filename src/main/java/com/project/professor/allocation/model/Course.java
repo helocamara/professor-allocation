c1ae19aabc002cea4ad16a5e74bfd7e84be90b83
Heloísa Câmara Bastos <heloisa.camara@hotmail.com>
2020-10-26 18:48:59 -0300

    create model classes

---
+++ src/main/java/com/project/professor/allocation/model/Course.java
@@ -0,0 +1,33 @@
+package com.project.professor.allocation.model;
+
+public class Course {
+	private Long id;
+	private String name;
+
+	public Course() {
+		super();
+	}
+
+	public Course(Long id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
